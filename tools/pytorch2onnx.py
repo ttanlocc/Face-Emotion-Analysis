a1b02a4739de2c86b7d706831902675c88c5c495
Tấn Lộc <81868486+ttanlocc@users.noreply.github.com>
2023-08-22 17:43:24 +0700

    turn off train

--- tools/pytorch2onnx.py
+++ tools/pytorch2onnx.py
@@ -1,155 +1,229 @@
-import argparse
-from functools import partial
+import warnings
+from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 
+import cv2
 import mmcv
-import numpy as np
-import onnxruntime as rt
 import torch
-from mmcv.onnx import register_extra_symbolics
-from mmcv.runner import load_checkpoint
-
-from mmcls.models import build_classifier
-
-torch.manual_seed(3)
-
-
-def _demo_mm_inputs(input_shape, num_classes):
-    """Create a superset of inputs needed to run test or train batches.
-
-    Args:
-        input_shape (tuple):
-            input batch dimensions
-        num_classes (int):
-            number of semantic classes
-    """
-    (N, C, H, W) = input_shape
-    rng = np.random.RandomState(0)
-    imgs = rng.rand(*input_shape)
-    gt_labels = rng.randint(
-        low=0, high=num_classes - 1, size=(N, 1)).astype(np.uint8)
-    mm_inputs = {
-        'imgs': torch.FloatTensor(imgs).requires_grad_(True),
-        'gt_labels': torch.LongTensor(gt_labels),
-    }
-    return mm_inputs
+import torch.distributed as dist
+import torch.nn as nn
+from mmcv import color_val
+from mmcv.utils import print_log
+from mmcv.runner import auto_fp16
 
 
-def pytorch2onnx(model,
-                 input_shape,
-                 opset_version=11,
-                 show=False,
-                 output_file='tmp.onnx',
-                 verify=False):
-    """Export Pytorch model to ONNX model and verify the outputs are same
-    between Pytorch and ONNX.
+class BaseClassifier(nn.Module, metaclass=ABCMeta):
+    """Base class for classifiers"""
 
-    Args:
-        model (nn.Module): Pytorch model we want to export.
-        input_shape (tuple): Use this input shape to construct
-            the corresponding dummy input and execute the model.
-        opset_version (int): The onnx op version. Default: 11.
-        show (bool): Whether print the computation graph. Default: False.
-        output_file (string): The path to where we store the output ONNX model.
-            Default: `tmp.onnx`.
-        verify (bool): Whether compare the outputs between Pytorch and ONNX.
-            Default: False.
-    """
-    model.cpu().eval()
+    def __init__(self):
+        super(BaseClassifier, self).__init__()
+        self.fp16_enabled = False
 
-    num_classes = model.head.num_classes
-    mm_inputs = _demo_mm_inputs(input_shape, num_classes)
+    @property
+    def with_neck(self):
+        return hasattr(self, 'neck') and self.neck is not None
 
-    imgs = mm_inputs.pop('imgs')
-    img_list = [img[None, :] for img in imgs]
+    @property
+    def with_head(self):
+        return hasattr(self, 'head') and self.head is not None
 
-    # replace original forward function
-    origin_forward = model.forward
-    model.forward = partial(model.forward, return_loss=False)
+    @abstractmethod
+    def extract_feat(self, imgs):
+        pass
 
-    register_extra_symbolics(opset_version)
-    with torch.no_grad():
-        torch.onnx.export(
-            model, (img_list, ),
-            output_file,
-            export_params=True,
-            keep_initializers_as_inputs=True,
-            verbose=show,
-            opset_version=opset_version)
-        print(f'Successfully exported ONNX model: {output_file}')
-    model.forward = origin_forward
+    def extract_feats(self, imgs):
+        assert isinstance(imgs, list)
+        for img in imgs:
+            yield self.extract_feat(img)
 
-    if verify:
-        # check by onnx
-        import onnx
-        onnx_model = onnx.load(output_file)
-        onnx.checker.check_model(onnx_model)
+    @abstractmethod
+    def forward_train(self, imgs, **kwargs):
+        """
+        Args:
+            img (list[Tensor]): List of tensors of shape (1, C, H, W).
+                Typically these should be mean centered and std scaled.
+            kwargs (keyword arguments): Specific to concrete implementation.
+        """
+        pass
 
-        # check the numerical value
-        # get pytorch output
-        pytorch_result = model(img_list, return_loss=False)[0]
+    @abstractmethod
+    def simple_test(self, img, **kwargs):
+        pass
 
-        # get onnx output
-        input_all = [node.name for node in onnx_model.graph.input]
-        input_initializer = [
-            node.name for node in onnx_model.graph.initializer
-        ]
-        net_feed_input = list(set(input_all) - set(input_initializer))
-        assert (len(net_feed_input) == 1)
-        sess = rt.InferenceSession(output_file)
-        onnx_result = sess.run(
-            None, {net_feed_input[0]: img_list[0].detach().numpy()})[0]
-        if not np.allclose(pytorch_result, onnx_result):
-            raise ValueError(
-                'The outputs are different between Pytorch and ONNX')
-        print('The outputs are same between Pytorch and ONNX')
+    @abstractmethod
+    def aug_test(self, img, **kwargs):
+        pass
 
+    def init_weights(self, pretrained=None):
+        if pretrained is not None:
+            print_log(f'load model from: {pretrained}', logger='root')
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Convert MMCls to ONNX')
-    parser.add_argument('config', help='test config file path')
-    parser.add_argument('--checkpoint', help='checkpoint file', default=None)
-    parser.add_argument('--show', action='store_true', help='show onnx graph')
-    parser.add_argument(
-        '--verify', action='store_true', help='verify the onnx model')
-    parser.add_argument('--output-file', type=str, default='tmp.onnx')
-    parser.add_argument('--opset-version', type=int, default=11)
-    parser.add_argument(
-        '--shape',
-        type=int,
-        nargs='+',
-        default=[224, 224],
-        help='input image size')
-    args = parser.parse_args()
-    return args
+    def forward_test(self, imgs, **kwargs):
+        """
+        Args:
+            imgs (List[Tensor]): the outer list indicates test-time
+                augmentations and inner Tensor should have a shape NxCxHxW,
+                which contains all images in the batch.
+        """
+        if isinstance(imgs, torch.Tensor):
+            imgs = [imgs]
+        for var, name in [(imgs, 'imgs')]:
+            if not isinstance(var, list):
+                raise TypeError(f'{name} must be a list, but got {type(var)}')
 
+        if len(imgs) == 1:
+            return self.simple_test(imgs[0], **kwargs)
+        else:
+            return self.aug_test(imgs, **kwargs)
+            # raise NotImplementedError('aug_test has not been implemented')
 
-if __name__ == '__main__':
-    args = parse_args()
+    @auto_fp16(apply_to=('img', ))
+    def forward(self, img, **kwargs):
+        """
+        Calls either forward_train or forward_test depending on whether
+        return_loss=True. Note this setting will change the expected inputs.
+        When `return_loss=True`, img and img_meta are single-nested (i.e.
+        Tensor and List[dict]), and when `resturn_loss=False`, img and img_meta
+        should be double nested (i.e.  List[Tensor], List[List[dict]]), with
+        the outer list indicating test time augmentations.
+        """
+        return_loss=False
+        if return_loss:
+            return self.forward_train(img, **kwargs)
+        else:
+            return self.forward_test(img, **kwargs)
 
-    if len(args.shape) == 1:
-        input_shape = (1, 3, args.shape[0], args.shape[0])
-    elif len(args.shape) == 2:
-        input_shape = (
-            1,
-            3,
-        ) + tuple(args.shape)
-    else:
-        raise ValueError('invalid input shape')
+    def _parse_losses(self, losses):
+        log_vars = OrderedDict()
+        for loss_name, loss_value in losses.items():
+            if isinstance(loss_value, torch.Tensor):
+                log_vars[loss_name] = loss_value.mean()
+            elif isinstance(loss_value, list):
+                log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
+            elif isinstance(loss_value, dict):
+                for name, value in loss_value.items():
+                    log_vars[name] = value
+            elif isinstance(loss_value, float) or isinstance(loss_value, int):
+                log_vars[loss_name] = torch.tensor(loss_value)
+            else:
+                raise TypeError(
+                    f'{loss_name} is not a tensor or list of tensors')
 
-    cfg = mmcv.Config.fromfile(args.config)
-    cfg.model.pretrained = None
+        loss = sum(_value for _key, _value in log_vars.items()
+                   if 'loss' in _key)
 
-    # build the model and load checkpoint
-    classifier = build_classifier(cfg.model)
+        log_vars['loss'] = loss
+        for loss_name, loss_value in log_vars.items():
+            # reduce loss when distributed training
+            if dist.is_available() and dist.is_initialized():
+                loss_value = loss_value.data.clone()
+                dist.all_reduce(loss_value.div_(dist.get_world_size()))
+            log_vars[loss_name] = loss_value.item()
 
-    if args.checkpoint:
-        load_checkpoint(classifier, args.checkpoint, map_location='cpu')
+        return loss, log_vars
 
-    # conver model to onnx file
-    pytorch2onnx(
-        classifier,
-        input_shape,
-        opset_version=args.opset_version,
-        show=args.show,
-        output_file=args.output_file,
-        verify=args.verify)
+    def train_step(self, data, optimizer):
+        """The iteration step during training.
+
+        This method defines an iteration step during training, except for the
+        back propagation and optimizer updating, which are done in an optimizer
+        hook. Note that in some complicated cases or models, the whole process
+        including back propagation and optimizer updating are also defined in
+        this method, such as GAN.
+
+        Args:
+            data (dict): The output of dataloader.
+            optimizer (:obj:`torch.optim.Optimizer` | dict): The optimizer of
+                runner is passed to ``train_step()``. This argument is unused
+                and reserved.
+
+        Returns:
+            dict: It should contain at least 3 keys: ``loss``, ``log_vars``,
+                ``num_samples``.
+                ``loss`` is a tensor for back propagation, which can be a
+                weighted sum of multiple losses.
+                ``log_vars`` contains all the variables to be sent to the
+                logger.
+                ``num_samples`` indicates the batch size (when the model is
+                DDP, it means the batch size on each GPU), which is used for
+                averaging the logs.
+        """
+        losses = self(**data)
+        loss, log_vars = self._parse_losses(losses)
+
+        outputs = dict(
+            loss=loss, log_vars=log_vars, num_samples=len(data['img'].data))
+
+        return outputs
+
+    def val_step(self, data, *args, **kwargs):
+        """The iteration step during validation.
+
+        This method shares the same signature as :func:`train_step`, but used
+        during val epochs. Note that the evaluation after training epochs is
+        not implemented with this method, but an evaluation hook.
+        """
+        losses = self(**data)
+        loss, log_vars = self._parse_losses(losses)
+
+        outputs = dict(
+            loss=loss, log_vars=log_vars, num_samples=len(data['img'].data))
+
+        return outputs
+
+    def show_result(self,
+                    img,
+                    result,
+                    text_color='green',
+                    font_scale=0.5,
+                    row_width=20,
+                    show=False,
+                    win_name='',
+                    wait_time=0,
+                    out_file=None):
+        """Draw `result` over `img`.
+
+        Args:
+            img (str or Tensor): The image to be displayed.
+            result (Tensor): The classification results to draw over `img`.
+            text_color (str or tuple or :obj:`Color`): Color of texts.
+            font_scale (float): Font scales of texts.
+            row_width (int): width between each row of results on the image.
+            show (bool): Whether to show the image.
+                Default: False.
+            win_name (str): The window name.
+            wait_time (int): Value of waitKey param.
+                Default: 0.
+            out_file (str or None): The filename to write the image.
+                Default: None.
+
+        Returns:
+            img (Tensor): Only if not `show` or `out_file`
+        """
+        img = mmcv.imread(img)
+        img = img.copy()
+
+        # write results on left-top of the image
+        x, y = 0, row_width
+        text_color = color_val(text_color)
+        for k, v in result.items():
+            if isinstance(v, float):
+                v = f'{v:.2f}'
+            label_text = f'{k}: {v}'
+            cv2.putText(img, label_text, (x, y), cv2.FONT_HERSHEY_COMPLEX,
+                        font_scale, text_color)
+            y += row_width
+
+        # if out_file specified, do not show image in window
+        if out_file is not None:
+            show = False
+
+        if show:
+            mmcv.imshow(img, win_name, wait_time)
+        if out_file is not None:
+            mmcv.imwrite(img, out_file)
+
+        if not (show or out_file):
+            warnings.warn('show==False and out_file is not specified, only '
+                          'result image will be returned')
+            return img
